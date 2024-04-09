bd15eb720c1ece0c59e6ddb9658449343cf477eb
Javi <javi@jsrois.net>
2024-04-09 14:51:05 +0200

    Reset de seguridad

--- backend/src/main/java/com/finalproject/backend/controllers/UserController.java
+++ backend/src/main/java/com/finalproject/backend/controllers/UserController.java
@@ -26,68 +26,16 @@ import com.finalproject.backend.repository.UserRepository;
 @RestController
 public class UserController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @PostMapping("/api/register")
-    public ResponseEntity<String> registerUser(@RequestBody AppUser user) {
-
-        AppUser existingUser = userRepository.findByEmail(user.getEmail());
-        if (existingUser != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("El correo ya esta en uso");
-        }
-
-        userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("User registered successfully");
+    public UserController(@Autowired UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
+
 
     @GetMapping("/api/users")
     public ResponseEntity<List<AppUser>> getUsers() {
         List<AppUser> users = userRepository.findAll();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        try {
-
-            AppUser user;
-
-            user = userRepository.findByEmail(loginRequest.getEmail());
-            
-            if (user == null) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-            }
-
-            if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            }
-
-            UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
-
-            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,
-                    loginRequest.getPassword(), userDetails.getAuthorities());
-
-            authenticationManager.authenticate(authentication);
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            return ResponseEntity.ok("Usuario autenticado");
-        } catch (BadCredentialsException e) {
-
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-    }
-
 }
